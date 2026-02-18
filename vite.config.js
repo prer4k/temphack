b59c4337a9e8f24b9b9bca8bf1b67f711f2f836b
Prerak Sharma <pwe@PwacOS.local>
2026-02-18 14:46:25 +0530

    allow ngrok [dev only]

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/api': { target: 'http://localhost:3001', changeOrigin: true },
+      "/api": { target: "http://localhost:3001", changeOrigin: true },
     },
+    allowedHosts: ["8cb7-2409-40d2-12b3-de30-9fc-1752-f0a7-116.ngrok-free.app"],
   },
 });
